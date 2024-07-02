746b28c1b7c8848416ec08f59ed8e9d33dfcdcc4
Varun Prakash <125754570+RaoVrn@users.noreply.github.com>
2024-07-02 20:59:45 +0530

    Add files via upload

---
+++ While Loop/1-to-100.py
@@ -0,0 +1,11 @@
+# Print numbers from 1 to 100.
+
+# Initialize a variable `i` to 1, which will be used as a counter.
+i = 1
+
+# Start a while loop that will continue to execute as long as `i` is less than or equal to 100.
+while (i <= 100):
+    # Print the current value of `i`.
+    print(i)
+    # Increment `i` by 1 at the end of each iteration.
+    i += 1
