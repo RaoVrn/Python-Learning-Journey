c6e185b53c32be932540451131e01a6cabe1bef2
Varun Prakash <125754570+RaoVrn@users.noreply.github.com>
2024-07-03 21:55:24 +0530

    Add files via upload

---
+++ While Loop/table_of_12.py
@@ -0,0 +1,11 @@
+# Print the multiplication table of the number 12.
+
+# Initialize a variable `i` to 1, which will be used as a counter.
+i = 1
+
+# Start a while loop that will continue to execute as long as `i` is less than or equal to 10.
+while (i <= 10):
+    # Print the multiplication of 12 with the current value of `i`.
+    print(12, "x", i, "=", 12*i)
+    # Increment `i` by 1 at the end of each iteration.
+    i += 1
